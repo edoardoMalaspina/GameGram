8822b882c8f51514a37c4c4bf20247605d6d7e4e
edoardoMalaspina <e.malaspina@studenti.unipi.it>
2023-04-27 11:06:44 +0200

    Added class to clean the price field in dataset

---
+++ src/main/java/com/example/gamegram/Cleaning.java
@@ -0,0 +1,65 @@
+package com.example.gamegram;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Cleaning {
+
+    public static void cleanTextPrices() {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("C:\\Users\\edoar\\Documents\\Università\\Large scale and Multistructured database\\datasets\\GAMES\\uncleanedPrices.csv"));
+            FileWriter myWriter = new FileWriter("pricesCleaned.csv");
+            String line = reader.readLine();
+            while (line != null) {
+                String s = line;
+                Pattern p = Pattern.compile("(\\p{Sc}) *([0-9.]*)");
+                Matcher m = p.matcher(s);
+                if (m.find()) {
+                    myWriter.write(m.group()+"\n");
+                }
+                else{
+                    myWriter.write("$0\n");
+                }
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createFinalPrices(){
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("pricesCleaned.csv"));
+            FileWriter myWriter = new FileWriter("pricesFinal.csv");
+            String line = reader.readLine();
+            while (line != null) {
+                String s = line;
+                if ( s.equals("$") || !s.startsWith("$") ){
+                    myWriter.write("$0\n");
+                }
+                else{
+                    myWriter.write(s + "\n");
+                }
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static void main(String[] args){
+        cleanTextPrices();
+        createFinalPrices();
+    }
+}
--- src/main/java/com/example/gamegram/UserManagerNeo4j.java
+++ src/main/java/com/example/gamegram/UserManagerNeo4j.java
@@ -85,6 +85,7 @@ public class UserManagerNeo4j {
     }
 
 
+
 }
 
 
