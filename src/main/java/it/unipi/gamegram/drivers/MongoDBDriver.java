0898bd9ed440d3c364596e8e8ace90a7be1f9057
edoardoMalaspina <e.malaspina@studenti.unipi.it>
2023-06-03 14:40:34 +0200

    corrected drivers

--- src/main/java/it/unipi/gamegram/drivers/MongoDBDriver.java
+++ src/main/java/it/unipi/gamegram/drivers/MongoDBDriver.java
@@ -9,24 +9,36 @@ import com.mongodb.client.MongoDatabase;
 public class MongoDBDriver {
 
     private static MongoDBDriver driver = null;
-    private final MongoDatabase database;
+    private MongoClient client;
+    private MongoDatabase database;
 
     private MongoDBDriver() {
-        MongoClient client = MongoClients.create("mongodb://localhost:27017/");
+        client = MongoClients.create("mongodb://10.1.1.14:27017," +
+                "10.1.1.13:27017," +
+                "10.1.1.12:27017" +
+                "/?retryWrites=true&w=2&wtimeoutMS=5000&readPreference=nearest");
         database = client.getDatabase("GameGram");
     }
 
     public static MongoDBDriver getInstance() {
-        if (driver == null)
+        if(driver == null)
             driver = new MongoDBDriver();
 
         return driver;
     }
 
     public MongoCollection<Document> getCollection(String collection) {
-        if (driver == null)
+        if(driver == null)
             throw new RuntimeException("No driver instance.");
         else
             return driver.database.getCollection(collection);
     }
+
+    public static void close() {
+        if(driver == null)
+            throw new RuntimeException("No driver instance.");
+        else
+            driver.client.close();
+    }
+
 }
